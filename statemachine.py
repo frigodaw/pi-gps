6999fd1108e6eb0150d78b204c82dedd48abf748
frigodaw <frigodaw@yahoo.com>
2019-04-29 23:43:00 +0200

    improved changing states in state machine

--- statemachine.py
+++ statemachine.py
@@ -9,10 +9,10 @@ from states import *
 
 #Screen objects
 states_list = []
-menu     =     Menu(0, "MENU",     0, [3,1], states_list)
-cycling  =  Cycling(1, "CYCLING",  0, [0,2], states_list)
-settings = Settings(2, "SETTINGS", 0, [1,3], states_list)
-exit     =   States(3, "EXIT",     0, [2,0], states_list)
+menu     =     Menu(0, "MENU",     0, [1,3], states_list)
+cycling  =  Cycling(1, "CYCLING",  0, [2,3], states_list)
+settings = Settings(2, "SETTINGS", 0, [0,3], states_list)
+exit     =     Exit(3, "EXIT",     0, [3], states_list)
 
 #Function to change screen content
 def start_state(state_nr):
@@ -26,16 +26,14 @@ def start_state(state_nr):
     return func()    
 
 #Initial assignment
-menu.set_active_flag()
-nr = menu.state_number
+initial_state = menu.state_number
+current_state = initial_state
 
 #Start of the state machine loop
 print("------- pi-gps 2019 -------")
 while True:
-    start_state(nr)
-    nr = int(input("Transition to: "))
-    if not nr in states_list:
-        nr = 0
+	start_state(current_state)
+	current_state = where_to()
     
 
 
--- states.py
+++ states.py
@@ -4,7 +4,12 @@ import serial
 import sys
 from pigps import *
 
+#Getter of dest variable. Used in process of changing state.
+def where_to():
+	return dest
+
 class States:
+    #Initial method which assigns given values to object.
     def __init__(self, state_number, header_name, active_flag, transitions_to = [], states_list = []):
         self.state_number = state_number
         self.header_name = header_name
@@ -12,25 +17,49 @@ class States:
         self.transitions_to = transitions_to
         states_list.append(self.state_number)
 
+    #Setter of object's active_flag varibale
     def set_active_flag(self):
         self.active_flag = 1
 
+    #Getter of object's active_flag varibale
     def reset_active_flag(self):
         self.active_flag = 0        
     
+    #Shows number of the active state
     def print_header(self):
         print("\n>>>  {}  <<<".format(self.header_name))
 
-    def print_content(self):
+    #Shows number of the active state
+    def print_screen_nr(self):
         print("Number of screen: {}".format(self.state_number))
+
+    #Shows elements of the list where legal transitions are stored
+    def print_transitions(self):
+        print("Available transitions to: {}".format(self.transitions_to))
+
+    #Read the number of demanding state from keyboard and returns it after validation
+    def transition(self):
+        global dest
+        try:
+            dest = int(input("Transition to: "))
+        except: 
+            ValueError      #To handle with incorrect input
+        if dest in self.transitions_to:
+            return dest
+        else:
+            return self.transition()
     
-    def _main(self):
-        self.set_active_flag()
+    #Execute print methods
+    def say_hello(self):
         self.print_header()
-        self.print_content()
-        print("Active flag before: {}".format(self.active_flag))
-        self.reset_active_flag()
-        print("Active flag after: {}".format(self.active_flag))
+        self.print_screen_nr()
+        self.print_transitions()
+
+
+    #Executes another methods
+    def _main(self):
+        self.say_hello()
+        self.transition()
 
 class Menu(States):
     pass
@@ -42,6 +71,9 @@ class Settings(States):
     pass
 
 class Exit(States):
-    pass
+    def _main(self):
+        self.say_hello()
+        print("\n------- pi-gps 2019 -------")
+        sys.exit()
 
 
